81df879cf53274fe1d038f5af6f9ad2991b65b81
Andreas Gehret <andreas.gehret@netfiles.de>
2020-09-03 22:49:16 +0200

    Extracted ui colors

---
+++ lib/ui/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+Map<int, Color> color =
+{
+  50:Color.fromRGBO(15,70,135, .1),
+  100:Color.fromRGBO(15,70,135, .2),
+  200:Color.fromRGBO(15,70,135, .3),
+  300:Color.fromRGBO(15,70,135, .4),
+  400:Color.fromRGBO(15,70,135, .5),
+  500:Color.fromRGBO(15,70,135, .6),
+  600:Color.fromRGBO(15,70,135, .7),
+  700:Color.fromRGBO(15,70,135, .8),
+  800:Color.fromRGBO(15,70,135, .9),
+  900:Color.fromRGBO(15,70,135, 1),
+};
+
+final MaterialColor colorDassu = MaterialColor(0xFF0D4789, color);
